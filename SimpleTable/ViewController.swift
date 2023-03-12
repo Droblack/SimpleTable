2b1ab1f9c24fe38f5a4683f343e4fcbf74eef314
Slava <sforce05@gmail.com>
2023-03-12 11:31:49 +0300

    Initial Commit

---
+++ SimpleTable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleTable
+//
+//  Created by Dr.Black on 11.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
